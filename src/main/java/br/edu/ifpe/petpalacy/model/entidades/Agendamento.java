3803e4dc3450d2cd5990db2ac34779d86cfb8f74
Pedro Henrique Resende Marques <pedro.marques@fitbank.com.br>
2023-06-27 15:02:30 -0300

    Introduce Parameter Object - 4

--- src/main/java/br/edu/ifpe/petpalacy/model/entidades/Agendamento.java
+++ src/main/java/br/edu/ifpe/petpalacy/model/entidades/Agendamento.java
@@ -1,60 +1,29 @@
-/*
-MIT License
-
-Copyright (c) 2018 Daniel da Silva Calado, Izaquiel Cavalcante da Silva, 
-                   Kaio Cesar Bezerra da Silva e Wemerson Diogenes da Silva
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
- */
 package br.edu.ifpe.petpalacy.model.entidades;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Date;
 import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
-/**
- *
- * @author Wemerson Diogenes da Silva <wemersondiogenes16@gmail.com>
- */
 @Entity
 public class Agendamento implements Serializable {
     
     @Id
     @GeneratedValue
     private Integer id;
-    @OneToOne(cascade={CascadeType.PERSIST,CascadeType.MERGE})
+    
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Servico servico;
-    @OneToOne(cascade={CascadeType.PERSIST,CascadeType.REFRESH}) 
+    
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH}) 
     private Cliente cliente;
+    
     private String data;
     private String hora;
+    
     @Enumerated(EnumType.ORDINAL)
     private StatusAgen statusAgen;
 
@@ -146,6 +115,4 @@ public class Agendamento implements Serializable {
     public String toString() {
         return "Agendamento{" + "id=" + id + ", servico=" + servico + ", cliente=" + cliente + ", data=" + data + ", hora=" + hora + ", statusAgen=" + statusAgen + '}';
     }
-
-
 }
